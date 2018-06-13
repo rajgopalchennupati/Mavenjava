76eb15e22daa0e3955dd06bd5b6632a4445cfac2
rajgopal <rajgopal.chennupati@gmail.com>
2018-06-13 14:25:03 -0400

    this is second commit

--- Mavenjava/src/test/java/qaclickacademy/AppiumTest.java
+++ Mavenjava/src/test/java/qaclickacademy/AppiumTest.java
@@ -1,5 +1,12 @@
 package qaclickacademy;
 
+import org.testng.annotations.Test;
+
 public class AppiumTest {
 
+	@Test
+	public void AppiumAutomation()
+	{
+		System.out.println("Iam in AppiumAutomation");
+	}
 }
