d8fb7ea1bee9b1caa4cf24d22e0145503a414e18
rajgopal <rajgopal.chennupati@gmail.com>
2018-06-13 14:20:30 -0400

    my first commit

---
+++ Mavenjava/src/test/java/qaclickacademy/RestApiTest.java
@@ -0,0 +1,13 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class RestApiTest {
+	
+	@Test
+	public void RestApiAutomation()
+	{
+		System.out.println("Iam in RestApiAutomation");
+	}
+
+}
---
+++ Mavenjava/src/test/java/qaclickacademy/SeleniumTest.java
@@ -0,0 +1,19 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	
+	@Test
+	public void BrowserAutomation()
+	{
+		System.out.println("Iam in SeleniumTest BrowserAutomation");
+	}
+	
+	@Test
+	public void sendMail()
+	{
+		System.out.println("Iam in SeleniumTest sendMail");
+	}
+
+}
